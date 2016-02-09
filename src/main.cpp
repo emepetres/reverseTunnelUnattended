4d08523518611f4a124d3a0af46cf36a72f38b7b
Javier Carnero <jcarnero@th-inc.es>
2016-02-09 11:56:50 +0100

    Main code formatted.

--- src/main.cpp
+++ src/main.cpp
@@ -44,30 +44,28 @@
 
 QString leerIdentificador(QString device);
 
-int main(int argc, char *argv[])
-{
-	QCoreApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+  QCoreApplication a(argc, argv);
 
-    //codificación de cadenas
-    QTextCodec *linuxCodec = QTextCodec::codecForName("UTF-8");
-    QTextCodec::setCodecForLocale(linuxCodec);
+  //codificación de cadenas
+  QTextCodec *linuxCodec = QTextCodec::codecForName("UTF-8");
+  QTextCodec::setCodecForLocale(linuxCodec);
 
-    ReverseTunnel * periodic_tunnel = ReverseTunnel::instance();
+  ReverseTunnel * periodic_tunnel = ReverseTunnel::instance();
 
-    periodic_tunnel->setConnectionData(HOST,USER,PASSWD,PORT);
+  periodic_tunnel->setConnectionData(HOST, USER, PASSWD, PORT);
 
-    periodic_tunnel->setHardwareId(leerIdentificador("/dev/sda"));
-    periodic_tunnel->setStatus("Hello!");
+  periodic_tunnel->setHardwareId(leerIdentificador("/dev/sda"));
+  periodic_tunnel->setStatus("Hello!");
 
-    if (periodic_tunnel->standaloneConnection(240))
-    {
-      qStdOut() << "Tunel abierto en screen \"reverse_tunnel\"; activo durante 4 minutos.\n\n"
-    			"CONECTESE LO ANTES POSIBLE. La sesión de screen no terminará hasta que se cierre manualmente o se apague el equipo.";
-    }
-    else
-    	qCritical() << "No se pudo abrir el tunel.";
+  if (periodic_tunnel->standaloneConnection(240)) {
+    qStdOut()
+        << "Tunel abierto en screen \"reverse_tunnel\"; activo durante 4 minutos.\n\n"
+            "CONECTESE LO ANTES POSIBLE. La sesión de screen no terminará hasta que se cierre manualmente o se apague el equipo.";
+  } else
+    qCritical() << "No se pudo abrir el tunel.";
 
-    return 0;
+  return 0;
 
 //    periodic_tunnel->start(600, 300);
 
@@ -76,49 +74,46 @@ int main(int argc, char *argv[])
 //    connection_times.push_back(QTime(16, 46));
 //    periodic_tunnel->start(connection_times, 60);
 
-    return a.exec();
+  return a.exec();
 }
 
-QString leerIdentificador(QString device)
-{
-	QString id;
+QString leerIdentificador(QString device) {
+  QString id;
 
-	QProcess proceso;
+  QProcess proceso;
 
-	//Ejecucion del proceso hwinfo
-	QString command = "hwinfo --disk --only %1";
-	command = command.arg(device);
-	proceso.start(command);
+  //Ejecucion del proceso hwinfo
+  QString command = "hwinfo --disk --only %1";
+  command = command.arg(device);
+  proceso.start(command);
 
-	if(!proceso.waitForStarted() || !proceso.waitForFinished())
-	{
-		proceso.close();
-		return "";
-	}
+  if (!proceso.waitForStarted() || !proceso.waitForFinished()) {
+    proceso.close();
+    return "";
+  }
 
-	QString salida = proceso.readAllStandardOutput();
-	proceso.close();
+  QString salida = proceso.readAllStandardOutput();
+  proceso.close();
 
-	//Filtrar la salida para obtener las lineas significantes
-	QStringList lineas_info;
-	QStringList lineas = salida.split('\n');
+  //Filtrar la salida para obtener las lineas significantes
+  QStringList lineas_info;
+  QStringList lineas = salida.split('\n');
 
-	QRegExp re_info("^\\s*(Model|Serial ID): (.*)$");
+  QRegExp re_info("^\\s*(Model|Serial ID): (.*)$");
 
-	for(int i = 0; i < lineas.length(); i++)
-	{
-		if(re_info.indexIn(lineas.at(i)) > -1)
-			lineas_info << re_info.cap(2);
-	}
+  for (int i = 0; i < lineas.length(); i++) {
+    if (re_info.indexIn(lineas.at(i)) > -1)
+      lineas_info << re_info.cap(2);
+  }
 
-	if(lineas_info.length() != 2)
-	{
-		qWarning() << QString("El dispositivo %1 no puede identificarse.").arg(device);
-		return "";
-	}
+  if (lineas_info.length() != 2) {
+    qWarning()
+        << QString("El dispositivo %1 no puede identificarse.").arg(device);
+    return "";
+  }
 
-	//Develolver el identificador: Unique ID - Parent ID
-	id = lineas_info.at(0) + " - " + lineas_info.at(1);
+  //Develolver el identificador: Unique ID - Parent ID
+  id = lineas_info.at(0) + " - " + lineas_info.at(1);
 
-	return id;
+  return id;
 }
